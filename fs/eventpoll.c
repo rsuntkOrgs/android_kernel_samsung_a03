e62275517f705053f6992d27125469a43f6552b0
Rissu J <rissu.ntk@gmail.com>
2024-10-16 10:21:52 +0000

    fs: eventpoll: Import changes from ACK

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1392,20 +1392,17 @@ static int ep_create_wakeup_source(struct epitem *epi)
 {
 	struct name_snapshot n;
 	struct wakeup_source *ws;
-
 	if (!epi->ep->ws) {
 		epi->ep->ws = wakeup_source_register(NULL, "eventpoll");
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
-
-	name = epi->ffd.file->f_path.dentry->d_name.name;
-	ws = wakeup_source_register(NULL, name);
-
+	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
+	ws = wakeup_source_register(NULL, n.name);
+	release_dentry_name_snapshot(&n);
 	if (!ws)
 		return -ENOMEM;
 	rcu_assign_pointer(epi->ws, ws);
-
 	return 0;
 }
 
